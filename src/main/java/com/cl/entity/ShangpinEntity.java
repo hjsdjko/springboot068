d8a33112cda58b0017467e3489ad2f75e29cdab2
jsxs <2261203961@qq.com>
2024-08-24 17:01:48 +0800

    first commit

---
+++ src/main/java/com/cl/entity/ShangpinEntity.java
@@ -0,0 +1,256 @@
+package com.cl.entity;
+
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.lang.reflect.InvocationTargetException;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.apache.commons.beanutils.BeanUtils;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.enums.FieldFill;
+import com.baomidou.mybatisplus.enums.IdType;
+
+
+/**
+ * 商品
+ * 数据库通用操作实体类（普通增删改查）
+ * @author 
+ * @email 
+ * @date 2024-04-05 11:32:11
+ */
+@TableName("shangpin")
+public class ShangpinEntity<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+
+	public ShangpinEntity() {
+		
+	}
+	
+	public ShangpinEntity(T t) {
+		try {
+			BeanUtils.copyProperties(this, t);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 主键id
+	 */
+	@TableId
+	private Long id;
+	/**
+	 * 商品标题
+	 */
+					
+	private String shangpinbiaoti;
+	
+	/**
+	 * 类型
+	 */
+					
+	private String leixing;
+	
+	/**
+	 * 服务
+	 */
+					
+	private String fuwu;
+	
+	/**
+	 * 品牌
+	 */
+					
+	private String pinpai;
+	
+	/**
+	 * 商品详情
+	 */
+					
+	private String shangpinxiangqing;
+	
+	/**
+	 * 商品图
+	 */
+					
+	private String shangpintu;
+	
+	/**
+	 * 收藏数量
+	 */
+					
+	private Integer storeupnum;
+	
+	/**
+	 * 最近点击时间
+	 */
+				
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat 		
+	private Date clicktime;
+	
+	/**
+	 * 积分
+	 */
+					
+	private Integer jf;
+	
+	/**
+	 * 价格
+	 */
+					
+	private Double price;
+	
+	
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat
+	private Date addtime;
+
+	public Date getAddtime() {
+		return addtime;
+	}
+	public void setAddtime(Date addtime) {
+		this.addtime = addtime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * 设置：商品标题
+	 */
+	public void setShangpinbiaoti(String shangpinbiaoti) {
+		this.shangpinbiaoti = shangpinbiaoti;
+	}
+	/**
+	 * 获取：商品标题
+	 */
+	public String getShangpinbiaoti() {
+		return shangpinbiaoti;
+	}
+	/**
+	 * 设置：类型
+	 */
+	public void setLeixing(String leixing) {
+		this.leixing = leixing;
+	}
+	/**
+	 * 获取：类型
+	 */
+	public String getLeixing() {
+		return leixing;
+	}
+	/**
+	 * 设置：服务
+	 */
+	public void setFuwu(String fuwu) {
+		this.fuwu = fuwu;
+	}
+	/**
+	 * 获取：服务
+	 */
+	public String getFuwu() {
+		return fuwu;
+	}
+	/**
+	 * 设置：品牌
+	 */
+	public void setPinpai(String pinpai) {
+		this.pinpai = pinpai;
+	}
+	/**
+	 * 获取：品牌
+	 */
+	public String getPinpai() {
+		return pinpai;
+	}
+	/**
+	 * 设置：商品详情
+	 */
+	public void setShangpinxiangqing(String shangpinxiangqing) {
+		this.shangpinxiangqing = shangpinxiangqing;
+	}
+	/**
+	 * 获取：商品详情
+	 */
+	public String getShangpinxiangqing() {
+		return shangpinxiangqing;
+	}
+	/**
+	 * 设置：商品图
+	 */
+	public void setShangpintu(String shangpintu) {
+		this.shangpintu = shangpintu;
+	}
+	/**
+	 * 获取：商品图
+	 */
+	public String getShangpintu() {
+		return shangpintu;
+	}
+	/**
+	 * 设置：收藏数量
+	 */
+	public void setStoreupnum(Integer storeupnum) {
+		this.storeupnum = storeupnum;
+	}
+	/**
+	 * 获取：收藏数量
+	 */
+	public Integer getStoreupnum() {
+		return storeupnum;
+	}
+	/**
+	 * 设置：最近点击时间
+	 */
+	public void setClicktime(Date clicktime) {
+		this.clicktime = clicktime;
+	}
+	/**
+	 * 获取：最近点击时间
+	 */
+	public Date getClicktime() {
+		return clicktime;
+	}
+	/**
+	 * 设置：积分
+	 */
+	public void setJf(Integer jf) {
+		this.jf = jf;
+	}
+	/**
+	 * 获取：积分
+	 */
+	public Integer getJf() {
+		return jf;
+	}
+	/**
+	 * 设置：价格
+	 */
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	/**
+	 * 获取：价格
+	 */
+	public Double getPrice() {
+		return price;
+	}
+
+}
